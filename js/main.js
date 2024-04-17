0c953f220d594414b02770ab2c447a78902fe23d
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-17 15:53:17 +0200

    durante

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-document.getElementById("age").valueOf
+let age=prompt ("età")
+
+let kilomiters=prompt ("Kilometri")
+
+if (age < 18){
+    let prezzo=(prezzo * 20 / 100 - prezzo)
+} else if (age > 65){
+    let prezzo=(prezzo * 40 / 100 - prezzo)
+}
+
+alert (prezzo * kilomiters)
+// console.log(`Il prezzo è: ${prezzo}`)
+
+// document.getElementById(biglietto).innerHTML=prezzo
+
+// document.getElementById("biglietto").innerHTML=()
